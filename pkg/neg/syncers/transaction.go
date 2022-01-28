25fea561ed8849f05acf6097a35ad3a46839c257
Bowei Du <bowei@google.com>
2022-01-28 11:14:02 -0800

    Fix unhandled err variable
    Remove defer from the mainline function

--- pkg/neg/syncers/transaction.go
+++ pkg/neg/syncers/transaction.go
@@ -165,15 +165,16 @@ func (s *transactionSyncer) sync() error {
 }
 
 func (s *transactionSyncer) syncInternal() error {
+	start := time.Now()
+	err := s.syncInternal2()
+	s.updateStatus(err)
+	metrics.PublishNegSyncMetrics(string(s.NegSyncerKey.NegType), string(s.endpointsCalculator.Mode()), err, start)
+	return err
+}
+
+func (s *transactionSyncer) syncInternal2() error {
 	s.syncLock.Lock()
 	defer s.syncLock.Unlock()
-
-	// NOTE: Error will be used to update the status on corresponding Neg CR if Neg CRD is enabled
-	// Please reuse and set err before returning
-	var err error
-	defer s.updateStatus(err)
-	start := time.Now()
-	defer metrics.PublishNegSyncMetrics(string(s.NegSyncerKey.NegType), string(s.endpointsCalculator.Mode()), err, start)
 
 	if s.needInit || s.isZoneChange() {
 		if err := s.ensureNetworkEndpointGroups(); err != nil {
@@ -218,6 +219,10 @@ func (s *transactionSyncer) syncInternal() error {
 		}
 		endpointsData := negtypes.EndpointsDataFromEndpointSlices(endpointSlices)
 		targetMap, endpointPodMap, err = s.endpointsCalculator.CalculateEndpoints(endpointsData, currentMap)
+		if err != nil {
+			err = fmt.Errorf("endpoints calculation error in mode %q, err: %w", s.endpointsCalculator.Mode(), err)
+			return err
+		}
 	} else {
 		ep, exists, err := s.endpointLister.Get(
 			&apiv1.Endpoints{
@@ -236,12 +241,12 @@ func (s *transactionSyncer) syncInternal() error {
 		}
 		endpointsData := negtypes.EndpointsDataFromEndpoints(ep.(*apiv1.Endpoints))
 		targetMap, endpointPodMap, err = s.endpointsCalculator.CalculateEndpoints(endpointsData, currentMap)
+		if err != nil {
+			err = fmt.Errorf("endpoints calculation error in mode %q, err: %w", s.endpointsCalculator.Mode(), err)
+			return err
+		}
 	}
 
-	if err != nil {
-		err = fmt.Errorf("endpoints calculation error in mode %q, err: %w", s.endpointsCalculator.Mode(), err)
-		return err
-	}
 	s.logStats(targetMap, "desired NEG endpoints")
 
 	// Calculate the endpoints to add and delete to transform the current state to desire state
@@ -268,9 +273,7 @@ func (s *transactionSyncer) syncInternal() error {
 	s.logEndpoints(addEndpoints, "adding endpoint")
 	s.logEndpoints(removeEndpoints, "removing endpoint")
 
-	// set err instead of returning directly so that synced condition on neg crd is properly updated in defer
-	err = s.syncNetworkEndpoints(addEndpoints, removeEndpoints)
-	return err
+	return s.syncNetworkEndpoints(addEndpoints, removeEndpoints)
 }
 
 // ensureNetworkEndpointGroups ensures NEGs are created and configured correctly in the corresponding zones.
