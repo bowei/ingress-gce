cf7ba9a32420ddff4c7ca306e826581a071437ba
Bowei Du <bowei@google.com>
2025-08-17 12:22:43 -0700

    getting closer

--- pkg/negbinding/controller_test.go
+++ pkg/negbinding/controller_test.go
@@ -17,28 +17,61 @@ limitations under the License.
 package negbinding
 
 import (
+	"context"
 	"testing"
 	"time"
 
+	gcpfirewallclient "github.com/GoogleCloudPlatform/gke-networking-api/client/gcpfirewall/clientset/versioned/fake"
+	networkclient "github.com/GoogleCloudPlatform/gke-networking-api/client/network/clientset/versioned/fake"
+	nodetopologyclient "github.com/GoogleCloudPlatform/gke-networking-api/client/nodetopology/clientset/versioned/fake"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-	v1 "k8s.io/ingress-gce/pkg/apis/negbinding/v1"
-	"k8s.io/ingress-gce/pkg/context"
+	"k8s.io/ingress-gce/pkg/apis/negbinding/v1"
+	backendconfigclient "k8s.io/ingress-gce/pkg/backendconfig/client/clientset/versioned/fake"
+	gce_context "k8s.io/ingress-gce/pkg/context"
+	frontendconfigclient "k8s.io/ingress-gce/pkg/frontendconfig/client/clientset/versioned/fake"
 	negbindingfake "k8s.io/ingress-gce/pkg/negbinding/clientset/versioned/fake"
+	serviceattachmentclient "k8s.io/ingress-gce/pkg/serviceattachment/client/clientset/versioned/fake"
+	svcnegclient "k8s.io/ingress-gce/pkg/svcneg/client/clientset/versioned/fake"
+	"k8s.io/klog/v2"
 )
 
-func newTestController() (*Controller, *context.ControllerContext) {
+func newTestController() *Controller {
 	kubeClient := fake.NewSimpleClientset()
 	negbindingClient := negbindingfake.NewSimpleClientset()
-	ctx, err := context.NewControllerContext(kubeClient, negbindingClient, nil, nil, nil, nil, nil, "", 0)
+	backendconfigClient := backendconfigclient.NewSimpleClientset()
+	frontendconfigClient := frontendconfigclient.NewSimpleClientset()
+	svcnegClient := svcnegclient.NewSimpleClientset()
+	serviceattachmentClient := serviceattachmentclient.NewSimpleClientset()
+	gcpfirewallClient := gcpfirewallclient.NewSimpleClientset()
+	networkClient := networkclient.NewSimpleClientset()
+	nodetopologyClient := nodetopologyclient.NewSimpleClientset()
+
+	ctx, err := gce_context.NewControllerContext(
+		kubeClient,
+		backendconfigClient,
+		frontendconfigClient,
+		gcpfirewallClient,
+		svcnegClient,
+		serviceattachmentClient,
+		networkClient,
+		nodetopologyClient,
+		kubeClient,
+		nil, // gce cloud
+		nil, // namer
+		"",  // kube system uid
+		gce_context.ControllerContextConfig{},
+		klog.TODO(),
+	)
 	if err != nil {
-		panic(err) // TODO: fixme
+		panic(err)
 	}
-	return NewController(ctx), ctx
+
+	return NewController(ctx, negbindingClient)
 }
 
 func TestController(t *testing.T) {
-	c, ctx := newTestController()
+	c := newTestController()
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 	go c.Run(stopCh)
@@ -56,20 +89,19 @@ func TestController(t *testing.T) {
 			NetworkEndpointGroups: []v1.NetworkEndpointGroupReference{},
 		},
 	}
-	_, err := c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Create(negBinding)
+	_, err := c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Create(context.TODO(), negBinding, metav1.CreateOptions{})
 	if err != nil {
 		t.Fatalf("Failed to create NetworkEndpointGroupBinding: %v", err)
 	}
-
 	// Update the NetworkEndpointGroupBinding resource
 	negBinding.Spec.ServiceRef.Name = "other-service"
-	_, err = c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Update(negBinding)
+	_, err = c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Update(context.TODO(), negBinding, metav1.UpdateOptions{})
 	if err != nil {
 		t.Fatalf("Failed to update NetworkEndpointGroupBinding: %v", err)
 	}
 
 	// Delete the NetworkEndpointGroupBinding resource
-	err = c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Delete(negBinding.Name, &metav1.DeleteOptions{})
+	err = c.negbindingClient.K8sV1().NetworkEndpointGroupBindings("default").Delete(context.TODO(), negBinding.Name, metav1.DeleteOptions{})
 	if err != nil {
 		t.Fatalf("Failed to delete NetworkEndpointGroupBinding: %v", err)
 	}
